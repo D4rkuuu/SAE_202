0c7ea3ef1be55f675ab0d0c2a699664a518fab74
CarpStew <e.appiah2007@gmail.com>
2026-03-31 11:51:19 +0200

    Début noeud_binaire

---
+++ noeud_binaire.py
@@ -0,0 +1,15 @@
+class NoeudBinaire:
+    """
+    Classe NoeudBinaire
+        Permet de créer un noeud binaire de manière récurssive
+    Attributs:
+    - valeur(Integer) : la valeur du noeud courant
+    - gauche(NoeudBinaire) : le sous-arbre gauche
+    - droit(NoeudBinaire) : le sous-arbre droit
+    """
+    nb_NoeudBinaire = 0
+
+    def __init__(self, valeur = None, gauche = None, droit = None):
+        self.valeur = valeur
+        self.gauche = gauche
+        self.droit = droit
