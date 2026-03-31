6244a9b526aaa8f982546e4960fe5a2551936b93
CarpStew <e.appiah2007@gmail.com>
2026-03-31 12:45:49 +0200

    NoeudBinaire : Ajout des mÃethodes booleens

--- noeud_binaire.py
+++ noeud_binaire.py
@@ -15,27 +15,46 @@ class NoeudBinaire:
     """
     nb_NoeudBinaire = 0
 
+# Constructeur
     def __init__(self, valeur : int | None = None, gauche : 'NoeudBinaire | None' = None, droit : 'NoeudBinaire | None' = None):
-        self.valeur = valeur
-        self.gauche = gauche
-        self.droit = droit
+        self.__valeur = valeur
+        self.__gauche = gauche
+        self.__droit = droit
         NoeudBinaire.nb_NoeudBinaire += 1
 
+# Getters/Setters
     def get_valeur(self):
-        return self.valeur
+        return self.__valeur
 
     def get_gauche(self):
-        return self.gauche
+        return self.__gauche
 
     def get_droit(self):
-        return self.droit
+        return self.__droit
 
     def set_valeur(self, valeur):
-        self.valeur = valeur
+        self.__valeur = valeur
 
     def set_gauche(self, gauche):
-        self.gauche = gauche
+        self.__gauche = gauche
 
     def set_droit(self, droit):
-        self.droit = droit
+        self.__droit = droit
 
+# Méthodes
+    def admet_sag(self):
+        if self.__gauche is not None:
+            return True
+        return False
+    def admet_sad(self):
+        if self.__droit is not None:
+            return True
+        return False
+    def feuille(self):
+        if self.__gauche and self.__droit is None:
+            return True
+        return False
+    def arbre_vide(self):
+        if self.__valeur and self.__gauche and self.__droit is  None:
+            return True
+        return False
