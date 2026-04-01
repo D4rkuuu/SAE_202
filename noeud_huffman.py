05f55060a98cc84d3eb64ed87fa7891c11d5b912
ak94u <akram.ben-abdallah@etu.u-pec.fr>
2026-04-01 12:44:09 +0200

    Huffman : debut

--- noeud_huffman.py
+++ noeud_huffman.py
@@ -1,3 +1,19 @@
 import sys
 import os
 import unicodedata as uni
+import noeud_binaire as nb
+
+class Huffman(nb):
+    def __init__(self, gauche, droit):
+        """(s) est la chaîne de caratère et (es) est 
+        le poids de la concaténation (s), 
+        - valeur devient un tuple 
+        - (s) position 0 plus le calcule de la concaténation 
+        - (es) position 1 avec le calcule du poids totale."""
+        
+        s = gauche.valeur[0] + droit.valeur[0]
+        es = gauche.valeur[1] + droit.valeur[1]
+        
+        super().__init_((s, es), gauche, droit)
+        
+       
